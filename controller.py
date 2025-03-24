3ba806399ea9497c8c70ead7456cb1df17b86b01
dalsassofilippo <s311881@studenti.polito.it>
2025-03-24 11:11:22 +0100

    soluzione fatta (in teoria)

--- controller.py
+++ controller.py
@@ -1,12 +1,13 @@
 import time
 import flet as ft
 import model as md
+from view import View
 
 class SpellChecker:
 
-    def __init__(self, view):
+    def __init__(self, v: View):
         self._multiDic = md.MultiDictionary()
-        self._view = view
+        self._view = v
 
     def handleSentence(self, txtIn, language, modality):
         txtIn = replaceChars(txtIn.lower())
@@ -20,7 +21,7 @@ class SpellChecker:
                 parole = self._multiDic.searchWord(words, language)
                 for parola in parole:
                     if not parola.corretta:
-                        paroleErrate = paroleErrate + str(parola) + " - "
+                        paroleErrate += str(parola) + " - "
                 t2 = time.time()
                 return paroleErrate, t2 - t1
 
@@ -29,7 +30,7 @@ class SpellChecker:
                 parole = self._multiDic.searchWordLinear(words, language)
                 for parola in parole:
                     if not parola.corretta:
-                        paroleErrate = paroleErrate + str(parola) + " "
+                        paroleErrate += str(parola) + " "
                 t2 = time.time()
                 return paroleErrate, t2 - t1
 
@@ -38,7 +39,7 @@ class SpellChecker:
                 parole = self._multiDic.searchWordDichotomic(words, language)
                 for parola in parole:
                     if not parola.corretta:
-                        paroleErrate = paroleErrate + str(parola) + " - "
+                        paroleErrate +=str(parola) + " - "
                 t2 = time.time()
                 return paroleErrate, t2 - t1
             case _:
@@ -56,6 +57,64 @@ class SpellChecker:
               "4. Exit\n" +
               "______________________________\n")
 
+    def handleSpellCheck(self,e):
+        # Controllo se tutti i campi sono stati selezionati
+        if self._view._menuLanguage.value is None:
+            self._view._messageTxt.value = "Errore: seleziona una lingua!"
+            self._view._messageTxt.color = "red"
+            self._view.page.update()
+            return
+        elif self._view._menuModality.value is None:
+            self._view._messageTxt.value = "Errore: seleziona un tipo di ricerca!"
+            self._view._messageTxt.color = "red"
+            self._view.page.update()
+            return
+        elif self._view._menuSentence.value.strip()=="":
+            self._view._messageTxt.value = "Errore: inserisci un testo da correggere!"
+            self._view._messageTxt.color = "red"
+            self._view.page.update()
+            return
+        else:
+            # Tutti i campi sono validi, esegui la correzione ortografica
+            incorrect_words, elapsed_time = self.handleSentence(
+                self._view._menuSentence.value,self._view._menuLanguage.value, self._view._menuModality.value
+            )
+            # Mostra i risultati nella ListView
+            self._view._txtOut.controls.append(ft.Text(f"Testo inserito: {self._view._menuSentence.value}"))
+            self._view._txtOut.controls.append(
+                ft.Text(f"Parole errate: {incorrect_words if incorrect_words else 'Nessun errore'}"))
+            self._view._txtOut.controls.append(ft.Text(f"Tempo di ricerca: {elapsed_time:.4f} sec"))
+            self._view._txtOut.controls.append(ft.Text(value="-------------------------------------------"))
+
+            # Svuota il campo di input
+            self._view._menuSentence.value = ""
+
+            self._view._messageTxt.value = "Correzione completata!"
+            self._view._messageTxt.color = "green"
+
+        self._view.page.update()
+
+    def handleLanguageSelection(self,e):
+        if self._view._menuLanguage.value!="":
+            self._view._messageTxt.value=f"Lingua selezionata: {self._view._menuLanguage.value}"
+            self._view._messageTxt.color="green"
+        else:
+            self._view._messageTxt.value = "Errore: seleziona una lingua!"
+            self._view._messageTxt.color = "red"
+            self._view.page.update()
+            return
+        self._view.page.update()
+
+    def handleModalitySelection(self,e):
+        if self._view._menuModality.value!="":
+            self._view._messageTxt.value=f"Modalità selezionata: {self._view._menuModality.value}"
+            self._view._messageTxt.color="green"
+        else:
+            self._view._messageTxt.value = "Errore: seleziona una modalità!"
+            self._view._messageTxt.color = "red"
+            self._view.page.update()
+            return
+        self._view.page.update()
 
 def replaceChars(text):
     chars = "\\`*_{}[]()>#+-.!$?%^;,=_~"
--- view.py
+++ view.py
@@ -1,4 +1,6 @@
 import flet as ft
+from flet.core.charts.chart_grid_lines import ChartGridLines
+
 
 class View(object):
     def __init__(self, page: ft.Page):
@@ -8,34 +10,60 @@ class View(object):
         self.page.horizontal_alignment = 'CENTER'
         self.page.theme_mode = ft.ThemeMode.LIGHT
         # Controller
-        self.__controller = None
+        self._controller = None
         # UI elements
-        self.__title = None
-        self.__theme_switch = None
-
+        self._title = None
+        self._theme_switch = None
+        self._menuLanguage = None
+        self._menuSentence = None
+        self._menuModality = None
+        self._btnCheck = None
+        self._txtOut = None
+        self._messageTxt = None
         # define the UI elements and populate the page
 
     def add_content(self):
         """Function that creates and adds the visual elements to the page. It also updates
         the page accordingly."""
         # title + theme switch
-        self.__title = ft.Text("TdP 2024 - Lab 04 - SpellChecker ++", size=24, color="blue")
-        self.__theme_switch = ft.Switch(label="Light theme", on_change=self.theme_changed)
-        self.page.controls.append(
-            ft.Row(spacing=30, controls=[self.__theme_switch, self.__title, ],
+        self._title = ft.Text("TdP 2024 - Lab 04 - SpellChecker ++", size=24, color="blue")
+        self._theme_switch = ft.Switch(label="Light theme", on_change=self.theme_changed)
+        row0=ft.Row(spacing=30, controls=[self._theme_switch, self._title, ],
                    alignment=ft.MainAxisAlignment.START)
-        )
+        #riga 1
+        self._menuLanguage=ft.Dropdown(label="Select language",
+                                        width=1600,
+                                        options=[
+                                            ft.dropdown.Option("english"),
+                                            ft.dropdown.Option("italian"),
+                                            ft.dropdown.Option("spanish")
+                                        ],
+                                        expand=True,
+                                        on_change=self._controller.handleLanguageSelection
+                                        )
+        row1=ft.Row(controls=[self._menuLanguage],alignment=ft.MainAxisAlignment.START)
+        #riga 2
+        self._menuModality=ft.Dropdown(label="Search modality",
+                                        options=[
+                                            ft.dropdown.Option("Default"),
+                                            ft.dropdown.Option("Linear"),
+                                            ft.dropdown.Option("Dichotomic")
+                                        ],expand=True,
+                                        on_change=self._controller.handleModalitySelection)
+        self._menuSentence=ft.TextField(label="Add your sentence here", width=900)
+        self._btnCheck=ft.ElevatedButton(text="Spell Check", on_click=self._controller.handleSpellCheck)
+        row2=ft.Row(controls=[self._menuModality,self._menuSentence,self._btnCheck],alignment=ft.MainAxisAlignment.START)
+        #riga 3
+        self._txtOut=ft.ListView(expand=1, spacing=10,padding=20, auto_scroll=True) #OUTPUT DELLA STAMPA
+        self._messageTxt=ft.Text(color="red") #PER GLI ERRORI
+        # separè tra ogni correzione e l'altra
 
-        # Add your stuff here
-
-        self.page.add([])
-
-        self.page.update()
+        self.page.add(row0,row1,row2,self._txtOut,self._messageTxt)
 
     def update(self):
         self.page.update()
     def setController(self, controller):
-        self.__controller = controller
+        self._controller = controller
     def theme_changed(self, e):
         """Function that changes the color theme of the app, when the corresponding
         switch is triggered"""
@@ -44,7 +72,7 @@ class View(object):
             if self.page.theme_mode == ft.ThemeMode.LIGHT
             else ft.ThemeMode.LIGHT
         )
-        self.__theme_switch.label = (
+        self._theme_switch.label = (
             "Light theme" if self.page.theme_mode == ft.ThemeMode.LIGHT else "Dark theme"
         )
         # self.__txt_container.bgcolor = (
